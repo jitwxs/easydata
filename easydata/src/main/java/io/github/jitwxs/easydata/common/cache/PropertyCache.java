bf793fcdd4426ede3260447ba9fd3fbe25c31564
jitwxs <jitwxs@foxmail.com>
2022-05-11 13:21:26 +0800

    add javadoc

--- easydata/src/main/java/io/github/jitwxs/easydata/common/cache/PropertyCache.java
+++ easydata/src/main/java/io/github/jitwxs/easydata/common/cache/PropertyCache.java
@@ -115,7 +115,7 @@ public class PropertyCache {
     /**
      * 受限于 java 自省的特性，对于 aB... 类型的字段，实际会处理成 AB...，需要做兼容处理
      * <p>
-     * 其中 a 表示任意小写字符，B 表示任意大写字符。例如 "sId" -> "SId"
+     * 其中 a 表示任意小写字符，B 表示任意大写字符。例如 "sId" will read "SId" in default
      *
      * @param field 字段名
      * @return 经过处理后的字段名
